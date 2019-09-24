ae74951ea8bd24b06c966ba6d3c58f29c423ebca
yuqu <yuquailon@gmail.com>
2019-09-24 00:26:39 -0700

    Add link context creation

--- src/commands/link/link.ts
+++ src/commands/link/link.ts
@@ -1,5 +1,31 @@
-import { LinkOptions } from "./interfaces";
+import { LinkOptions, LinkContext, LinkConfig } from "./interfaces";
+import { linkConfigSchema } from "./configSchema";
+import * as fileUtil from "../../util/file";
 
-export function link(options: LinkOptions): void {
-    console.log(JSON.stringify(options));
-}
+const getLinkConfig = (filePath: string): LinkConfig => {
+    const file = fileUtil.readJsonFile(filePath);
+
+    const { error, value } = linkConfigSchema.validate(file, { stripUnknown: true });
+
+    if (error) {
+        throw error;
+    }
+
+    return value as LinkConfig;
+};
+
+const link = (options: LinkOptions): void => {
+    const { configFile } = options;
+
+    const linkConfig = getLinkConfig(configFile);
+    const fileList = fileUtil.getAllFilesFromFolder(options.workingDirectory);
+
+    const context: LinkContext = {
+        fileList,
+        linkConfig
+    };
+
+    console.log(context);
+};
+
+export { link };
