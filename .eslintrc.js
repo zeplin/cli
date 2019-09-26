76d02e4a5650b1c0d77bda685bd68dd0ffad3c83
Yucel Okcu <yuquailon@gmail.com>
2019-09-25 17:42:33 -0700

    Fix eslint parsing error

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        'project': './tsconfig.json'
+        "ecmaVersion": 2018,
+        "sourceType": "module"
     },
     plugins: ['@typescript-eslint'],
     rules: {
