4a434d5cf6fe79edbc398bf9f5520574833d8d46
Yucel Okcu <yuquailon@gmail.com>
2019-10-01 14:29:41 +0300

    Change file utilities to async

--- src/commands/link/index.ts
+++ src/commands/link/index.ts
@@ -1,2 +1,36 @@
-export * from "./interfaces";
-export * from "./link";
+import { LinkContext, LinkConfig } from "./interfaces";
+import { linkConfigSchema } from "./configSchema";
+import * as fileUtil from "../../util/file";
+
+const getLinkConfig = async (filePath: string): Promise<LinkConfig> => {
+    const file = await fileUtil.readJsonFile(filePath);
+
+    const { error, value } = linkConfigSchema.validate(file, { stripUnknown: true });
+
+    if (error) {
+        throw error;
+    }
+
+    return value as LinkConfig;
+};
+
+export interface LinkOptions {
+    configFile: string;
+    devMode: boolean;
+    workingDirectory: string;
+}
+
+export async function link(options: LinkOptions): Promise<void> {
+    const { configFile } = options;
+
+    const linkConfig = await getLinkConfig(configFile);
+    const fileList = await fileUtil.getAllFilesFromFolder(options.workingDirectory);
+
+    const context: LinkContext = {
+        fileList,
+        linkConfig
+    };
+
+    console.log(context);
+}
+
