e9b1614a194fe048fe9f37a6c0ee75201c5a2afa
Yucel Okcu <yuquailon@gmail.com>
2019-10-01 14:38:00 +0300

    Refactor commandRunner to be more human readable

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,15 +11,16 @@ module.exports = {
         "plugin:@typescript-eslint/recommended"
     ],
     parser: '@typescript-eslint/parser',
-    parserOptions:  {
+    parserOptions: {
         'project': './tsconfig.json'
-      },
+    },
     plugins: ['@typescript-eslint'],
     rules: {
         "capitalized-comments": "error",
         "arrow-body-style": ["error", "as-needed"],
         "no-sync": "off",
         "no-process-exit": "off",
-        "no-process-env": "off"
+        "no-process-env": "off",
+        "@typescript-eslint/no-explicit-any": ["error", { "ignoreRestArgs": true }]
     }
 };
