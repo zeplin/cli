e73ad6f96a936def2ad4df3d85d3129d6c280e08
Yucel Okcu <yuquailon@gmail.com>
2019-10-14 21:46:06 +0300

    Fix file reading

--- src/util/file.ts
+++ src/util/file.ts
@@ -2,11 +2,12 @@ import fs from "fs-extra";
 import path from "path";
 
 export async function readJsonFile(filePath: string): Promise<{}> {
-    if (!(await fs.pathExists(filePath))) {
+    const resolvedFilePath = path.resolve(filePath);
+    if (!(await fs.pathExists(resolvedFilePath))) {
         throw new Error(`Cannot access file: ${filePath}`);
     }
 
-    return fs.readJson(filePath);
+    return fs.readJson(resolvedFilePath);
 }
 
 export async function writeJsonIntoFile(filePath: string, content: {}): Promise<void> {
@@ -16,9 +17,11 @@ export async function writeJsonIntoFile(filePath: string, content: {}): Promise<
 export async function getAllFilesFromFolder(folderPath: string): Promise<string[]> {
     let results: string[] = [];
 
-    const folderContent = await fs.readdir(folderPath);
+    const resolvedFolderPath = path.resolve(folderPath);
+
+    const folderContent = await fs.readdir(resolvedFolderPath);
     await Promise.all(folderContent.map(async file => {
-        const filePath = path.join(folderPath, file);
+        const filePath = path.resolve(resolvedFolderPath, file);
 
         const stat = await fs.stat(filePath);
         if (stat && stat.isDirectory()) {
