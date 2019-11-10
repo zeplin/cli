8a0c92810802b8a2545695a6c9c05e927146f59a
Yucel Okcu <yuquailon@gmail.com>
2019-11-10 17:44:20 +0300

    Hide 401 APIError message

--- src/errors/APIError.ts
+++ src/errors/APIError.ts
@@ -1,13 +1,26 @@
 import { AxiosResponse } from "axios";
+import { UNAUTHORIZED } from "http-status-codes";
 
 export class APIError extends Error {
-    constructor(response: AxiosResponse) {
-        let message = `API Error: ${response.status}: ${response.statusText}`;
+    status: number;
 
-        if (response.data) {
-            message = `${response.status}: ${response.data.title}: ${response.data.message}`;
+    constructor(response: AxiosResponse) {
+        let message;
+
+        if (response.status === UNAUTHORIZED) {
+            message = `API Error: ${response.status} - Authentication token is invalid.`;
+        } else if (response.data) {
+            message = `API Error: ${response.status} - ${response.data.title}: ${response.data.message}`;
+        } else {
+            message = `API Error: ${response.status} - ${response.statusText}`;
         }
 
         super(message);
+
+        this.status = response.status;
+    }
+
+    static isAPIError(err: Error): err is APIError {
+        return err instanceof APIError;
     }
 }
