0368664dbe4f00067b769e9a95759d62557af2be
Yucel Okcu <yuquailon@gmail.com>
2019-11-13 17:09:10 +0300

    Fix default value for config file

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,11 @@ import { commandRunner } from "./util/command";
 const program = new commander.Command();
 
 function collectionValue(value: string, previous: string[]): string[] {
+    // Replace default value
+    if (previous.length === 1 && previous[0] === defaults.commands.connect.filePath[0]) {
+        return [value];
+    }
+
     return previous.concat([value]);
 }
 
@@ -21,25 +26,19 @@ console.log(`Zeplin CLI - v${version}\n`);
 const connectCommand = program.command("connect");
 
 connectCommand.description("Connect components to code")
-    .option("-f, --file <file>", "Full path to components config file", collectionValue, [])
+    .option("-f, --file <file>", "Full path to components file", collectionValue, defaults.commands.connect.filePath)
     .option("-d, --dev-mode", "Activate development mode", defaults.commands.connect.devMode)
     .option("--port <port>", "Optional port number for development mode", defaults.commands.connect.port)
     .option("-p, --plugin <plugin>", "NPM package name of a Zeplin CLI connect plugin", collectionValue, [])
     .action(commandRunner(async options => {
-        if (options.file.length === 0) {
-            console.error("Hey hey! We need a component config file to connect components!");
+        const connectOptions: ConnectOptions = {
+            configFiles: options.file,
+            devMode: options.devMode,
+            plugins: options.plugin,
+            devModePort: options.port
+        };
 
-            connectCommand.outputHelp();
-        } else {
-            const connectOptions: ConnectOptions = {
-                configFiles: options.file,
-                devMode: options.devMode,
-                plugins: options.plugin,
-                devModePort: options.port
-            };
-
-            await connect(connectOptions);
-        }
+        await connect(connectOptions);
     }));
 
 program.on("command:*", () => {
