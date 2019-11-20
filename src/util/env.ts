02d43849e45a78de8d6faee34f68b55b56829868
Yucel Okcu <yuquailon@gmail.com>
2019-11-20 21:21:48 +0300

    Add verbose flag

--- src/util/env.ts
+++ src/util/env.ts
@@ -15,11 +15,21 @@ const CI_ENV_KEYS = [
     "GITHUB_ACTIONS"
 ];
 
+let VERBOSE = false;
+
+const isVerbose = (): boolean => VERBOSE;
+
+const activateVerbose = (): void => {
+    VERBOSE = true;
+};
+
 const isCI = (): boolean => CI_ENV_KEYS.some(key => key in process.env);
 
 const getAccessTokenFromEnv = (): string | undefined => process.env.ZEPLIN_ACCESS_TOKEN;
 
 export {
+    isVerbose,
+    activateVerbose,
     isCI,
     getAccessTokenFromEnv
 };
