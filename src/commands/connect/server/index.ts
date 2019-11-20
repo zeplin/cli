ad48aad57795c31011ed29b5bc82aa8d5f9ba328
Yucel Okcu <yuquailon@gmail.com>
2019-11-20 11:36:19 +0300

    Update cors settings

--- src/commands/connect/server/index.ts
+++ src/commands/connect/server/index.ts
@@ -24,7 +24,8 @@ export class ConnectDevServer {
         // CORS
         app.use((_req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            res.header("Access-Control-Allow-Headers", "X-Requested-With, Origin");
+            res.header("Access-Control-Allow-Methods", "GET");
             next();
         });
 
