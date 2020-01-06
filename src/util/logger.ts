67207be900f7850b24ee6e53573fe678e771cadc
Yucel Okcu <yucel.n.okcu@gmail.com>
2020-01-06 11:45:44 +0300

    Add winston logger

---
+++ src/util/logger.ts
@@ -0,0 +1,34 @@
+import winston, { format } from "winston";
+import os from "os";
+import stripAnsi from "strip-ansi";
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.File({
+            level: "silly",
+            filename: "cli.log",
+            maxsize: 1048576, // 1 MB
+            maxFiles: 2,
+            dirname: `${os.homedir()}/.zeplin`,
+            tailable: true,
+            format: format.combine(
+                format.timestamp({
+                    format: "YYYY-MM-DD HH:mm:ss"
+                }),
+                format.errors({ stack: true }),
+                format.splat(),
+                format.printf(m => stripAnsi(`${m.timestamp} - ${m.level} - ${m.message}${m.stack ? `\n${m.stack}` : ""}`))
+            )
+        }),
+        new winston.transports.Console({
+            level: "info",
+            stderrLevels: ["error"],
+            format: format.combine(
+                format.splat(),
+                format.printf(m => `${m.message}`)
+            )
+        })
+    ]
+});
+
+export default logger;
