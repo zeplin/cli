6f299503239002288435daacb6b9ee15d208e53f
Yucel Okcu <yucel.n.okcu@gmail.com>
2020-11-23 16:54:46 +0300

    Fix plugin installation instructions
    We were suggesting to use npm global install even if the user has
    yarn and/or local installation. This commit will detect the runtime
    and show installation instrucitons accordingly.

--- src/util/text.ts
+++ src/util/text.ts
@@ -1,9 +1,24 @@
+import { isRunningFromYarnGlobal, isRunningFromGlobal, projectHasYarn } from "./package";
+
 const indentRegex = /^(?!\s*$)/gm;
 const indentCount = 4;
 
 const indent = (multiLineText: string, opts: { character: string } = { character: " " }): string =>
     multiLineText.replace(indentRegex, opts.character.repeat(indentCount));
 
+const getInstallCommand = (packageName: string): string => {
+    if (isRunningFromYarnGlobal()) {
+        return `yarn global add ${packageName}`;
+    } else if (isRunningFromGlobal()) {
+        return `npm install -g ${packageName}`;
+    } else if (projectHasYarn()) {
+        return `yarn add ${packageName}`;
+    }
+
+    return `npm install ${packageName}`;
+};
+
 export {
-    indent
+    indent,
+    getInstallCommand
 };
