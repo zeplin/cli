0b650abffea92003e45ef3720c2ee5bed10c0221
Yucel Okcu <yucel.n.okcu@gmail.com>
2020-12-01 16:53:14 +0300

    Use utf-8 encoding on file read/write

--- src/util/file.ts
+++ src/util/file.ts
@@ -1,15 +1,23 @@
 import fs from "fs-extra";
 import path from "path";
 
-export async function readJsonFile(filePath: string): Promise<{}> {
+async function readJsonFile(filePath: string): Promise<{}> {
     const resolvedFilePath = path.resolve(filePath);
     if (!(await fs.pathExists(resolvedFilePath))) {
         throw new Error(`Cannot access file: ${filePath}`);
     }
 
-    return fs.readJson(resolvedFilePath);
+    return fs.readJson(resolvedFilePath, { encoding: "utf-8" });
 }
 
-export async function writeJsonIntoFile(filePath: string, content: {}): Promise<void> {
-    await fs.writeFile(filePath, JSON.stringify(content));
+async function writeJsonIntoFile(filePath: string, content: {}): Promise<void> {
+    await fs.writeJson(filePath, content, {
+        encoding: "utf-8",
+        spaces: 2
+    });
 }
+
+export {
+    readJsonFile,
+    writeJsonIntoFile
+};
