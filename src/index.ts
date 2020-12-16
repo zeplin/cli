6ba0e1430a98b37a766dd37b482c6ad2e633b3b3
Helehelehele <mert.kizilirmak@zeplin.io>
2020-12-16 16:10:02 +0300

    Remove debugging code

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,3 @@ import * as commands from "./commands";
 export {
     commands
 };
-
-/*
-import { LoginAuthServer } from "./commands/login/server";
-
-const server = new LoginAuthServer("/authorize");
-
-async function run() {
-    const token = await server.waitForToken({ port: 8080 });
-    console.log(token);
-}
-
-run();
-*/
