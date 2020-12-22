567f7acbbae5cf8b3b0af077f8b20a51ef7f40b2
Helehelehele <mert.kizilirmak@zeplin.io>
2020-12-22 16:45:27 +0300

    Use catch instead of then inside mockRejectedWithValue

--- test/util.ts
+++ test/util.ts
@@ -2,6 +2,6 @@ const mockResolvedWithValue = (mocked: jest.Mock, value: unknown, nth = 0): Prom
     mocked.mock.results[nth].value.then((result: unknown) => expect(result).toEqual(value));
 
 const mockRejectedWithValue = (mocked: jest.Mock, value: unknown, nth = 0): Promise<void> =>
-    mocked.mock.results[nth].value.then((result: unknown) => expect(result).toEqual(value));
+    mocked.mock.results[nth].value.catch((result: unknown) => expect(result).toEqual(value));
 
 export { mockResolvedWithValue, mockRejectedWithValue };
