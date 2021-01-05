74e6c20d48e99312f27b016d1d915cce8756a393
Yucel Okcu <yucel.n.okcu@gmail.com>
2021-01-05 16:11:53 +0300

    Add missing type annotations

--- src/tasks/context/resource.d.ts
+++ src/tasks/context/resource.d.ts
@@ -1,16 +1,19 @@
 import { AuthenticationContext } from "./authentication";
 
+/** @internal */
 export interface Organization {
     _id: string;
     name: string;
 }
 
+/** @internal */
 export interface ZeplinComponent {
     _id: string;
     sourceId: string;
     name: string;
 }
 
+/** @internal */
 export interface ZeplinResource {
     _id: string;
     name: string;
@@ -18,6 +21,7 @@ export interface ZeplinResource {
     organization?: Organization;
 }
 
+/** @internal */
 export interface ResourceContext extends AuthenticationContext {
     projectId: string;
     styleguideId: string;
