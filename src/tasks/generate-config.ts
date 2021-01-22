616076b6d97790217d901fbedb79125533f30da8
Yucel Okcu <yucel.n.okcu@gmail.com>
2021-01-22 15:52:32 +0300

    Exec and merge all configurators of plugins

--- src/tasks/generate-config.ts
+++ src/tasks/generate-config.ts
@@ -3,51 +3,93 @@ import * as ui from "./ui/generate-config";
 import { InitializeContext } from "./context/initialize";
 import { mkdir, writeJsonIntoFile } from "../util/file";
 import path from "path";
-import { ComponentConfigFile } from "../commands/connect/interfaces/config";
+import { ComponentConfigFile, Plugin } from "../commands/connect/interfaces/config";
 import logger from "../util/logger";
 import { stringify } from "../util/text";
+import { SupportedProjectType } from "../service/project-type/project-types";
+import { PackageJson } from "../util/js/config";
 
 const writeFile = async (filePath: string, config: ComponentConfigFile): Promise<void> => {
     await mkdir(path.dirname(filePath));
     await writeJsonIntoFile(filePath, config);
 };
 
-const generate: TaskStep<InitializeContext> = async ctx => {
-    const config: ComponentConfigFile = Object.create(null);
-    const resource = ctx.selectedResource;
-
-    config.plugins = await Promise.all(
-        ctx.installedPlugins.map(async name => {
-            const projectType = ctx.projectTypes.find(pt =>
-                pt.installPackages && pt.installPackages.includes(name)
-            );
-
-            if (projectType?.configurator) {
-                const pluginConfig = await projectType.configurator(ctx.packageJson);
-                return { name, config: pluginConfig };
+const executeConfigurators = async (
+    pluginName: string,
+    projectTypes: SupportedProjectType[],
+    packageJson: PackageJson | null
+): Promise<Array<{}>> => {
+    const pluginConfigs = await Promise.all(
+        projectTypes.filter(pt =>
+            pt.installPackages && pt.installPackages.includes(pluginName)
+        ).map(async pt => {
+            if (pt.configurator) {
+                const pluginConfig = await pt.configurator(packageJson);
+                return pluginConfig;
             }
-
-            return { name };
+            return null;
         })
     );
+    return pluginConfigs.filter(Boolean) as Array<{}>;
+};
 
-    if (resource.type === "Project") {
-        config.projects = [resource._id];
+const getPluginConfigs = (
+    installedPlugins: string[],
+    projectTypes: SupportedProjectType[],
+    packageJson: PackageJson | null
+): Promise<Plugin[]> => Promise.all(
+    installedPlugins.map(async name => {
+        const configs = await executeConfigurators(name, projectTypes, packageJson);
+
+        if (configs.length > 0) {
+            const pluginConfig = configs.reduce(
+                (prev, curr) => Object.assign(prev, curr), {}
+            );
+            return {
+                name,
+                config: pluginConfig
+            };
+        }
+
+        return { name };
+    })
+);
+
+const generate: TaskStep<InitializeContext> = async ctx => {
+    const {
+        installedPlugins = [],
+        projectTypes = [],
+        packageJson,
+        selectedResource,
+        selectedComponents,
+        file,
+        git,
+        cliOptions: {
+            configFile
+        }
+    } = ctx;
+
+    const config: ComponentConfigFile = Object.create(null);
+
+    config.plugins = await getPluginConfigs(installedPlugins, projectTypes, packageJson);
+
+    if (selectedResource.type === "Project") {
+        config.projects = [selectedResource._id];
     } else {
-        config.styleguides = [resource._id];
+        config.styleguides = [selectedResource._id];
     }
 
     config.components = [{
-        path: ctx.file.path,
-        zeplinIds: ctx.selectedComponents.map(c => c._id)
+        path: file.path,
+        zeplinIds: selectedComponents.map(c => c._id)
     }];
 
-    if (ctx.git) {
-        config[ctx.git.type] = ctx.git.config;
+    if (git) {
+        config[git.type] = git.config;
     }
 
     logger.debug(`Generated config file: ${stringify(config)}`);
-    await writeFile(ctx.cliOptions.configFile, config);
+    await writeFile(configFile, config);
 };
 
 export const generateConfig = new Task<InitializeContext>({
