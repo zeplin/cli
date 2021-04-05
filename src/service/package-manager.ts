b4f6a90bbf3534ef1d8f05b918f47ca938e06401
Yucel Okcu <yucel.n.okcu@gmail.com>
2021-04-05 16:16:51 +0300

    Fallback to `npm view` if latest-version fails

--- src/service/package-manager.ts
+++ src/service/package-manager.ts
@@ -1,12 +1,40 @@
-import latestVersion from "latest-version";
 import hasYarn from "has-yarn";
+import latestVersion from "latest-version";
+import semver from "semver";
+import { CLIError } from "../errors/CLIError";
 import { runCommand } from "../util/command";
+import logger from "../util/logger";
+
+async function getLatestVersionUsingNpm(packageName: string): Promise<string> {
+    try {
+        const out = await runCommand(`npm view ${packageName} versions --json`) || "[]";
+        const versions = Array.from<string>(JSON.parse(out));
+        const latestStable = versions.reverse().find(v => semver.parse(v)?.prerelease.length === 0);
+        if (!latestStable) {
+            throw new CLIError("Could not find latest version on the list");
+        }
+
+        return latestStable;
+    } catch (err) {
+        logger.debug(err);
+        throw new CLIError("Could not get latest version");
+    }
+}
+
+function getLatestVersion(packageName: string): Promise<string> {
+    try {
+        return latestVersion(packageName);
+    } catch (err) {
+        logger.debug(err);
+        return getLatestVersionUsingNpm(packageName);
+    }
+}
 
 export async function getLatestVersions(packages: string[]): Promise<Record<string, string>> {
     const versions: Record<string, string> = {};
 
     await Promise.all(packages.map(async p => {
-        versions[p] = await latestVersion(p);
+        versions[p] = await getLatestVersion(p);
     }));
 
     return versions;
